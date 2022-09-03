5520825733fc08d75d595d02e5deada847a81e79
Chensn <onlottlr@gmail.com>
2022-09-03 17:58:25 +0800

    藝文場館連Json資料

--- JavaScript/藝文場館.js
+++ JavaScript/藝文場館.js
@@ -1,5 +1,5 @@
-
-
+// ===================================================================================北部
+// 網頁載入先跑出圖片圖片有加一個click事件
 fetch('./text場館.json')
     .then(function (response) {
         return response.json();
@@ -15,10 +15,9 @@ fetch('./text場館.json')
             <div class="col-lg-2">
                 <button class="openBox" onclick="OpenModal(${element.場館編號})">
                     <img src="${element.場館圖片}" title="${element.場館名稱}">
-                    <p>${element.場館編號}${element.場館名稱}</p>
+                    <p>${element.場館名稱}</p>
                 </button>
             </div>
-            
             `;
         });
         showVenueNorth.innerHTML += html;
@@ -26,6 +25,7 @@ fetch('./text場館.json')
 
 
 function OpenModal(el) {
+
 
     let element = document.getElementById('overlay')
     element.style.display = 'block'
@@ -53,22 +53,15 @@ function OpenModal(el) {
             <div class="relativeForLabel">
                 <select class="selectDay" id="selectDay">
                     <option value="營業時間查詢">請選擇</option>
-                    <option value="#tab_Mon">星期一</option>
-                    <option value="#tab_Tue">星期二</option>
-                    <option value="#tab_Wed">星期三</option>
-                    <option value="#tab_Thr">星期四</option>
-                    <option value="#tab_Fri">星期五</option>
-                    <option value="#tab_Sat">星期六</option>
-                    <option value="#tab_Sun">星期日</option>
+                    <option value="tab_Mon">星期一</option>
+                    <option value="tab_Tue">星期二</option>
+                    <option value="tab_Wed">星期三</option>
+                    <option value="tab_Thr">星期四</option>
+                    <option value="tab_Fri">星期五</option>
+                    <option value="tab_Sat">星期六</option>
+                    <option value="tab_Sun">星期日</option>
                 </select>
-
-                <div id="tab_Mon"><label>${myJson[el - 1].星期一}</label></div>
-                <div id="tab_Tue"><label>${myJson[el - 1].星期二}</label></div>
-                <div id="tab_Wed"><label>${myJson[el - 1].星期三}</label></div>
-                <div id="tab_Thr"><label>${myJson[el - 1].星期四}</label></div>
-                <div id="tab_Fri"><label>${myJson[el - 1].星期五}</label></div>
-                <div id="tab_Sat"><label>${myJson[el - 1].星期六}</label></div>
-                <div id="tab_Sun"><label>${myJson[el - 1].星期日}</label></div>
+                <div><label>這邊要發生change事件後才會被新增</label></div>
             </div>
             </p>`
 
@@ -76,29 +69,49 @@ function OpenModal(el) {
             }
 
             vVenueText.innerHTML = html;
+
+            let selectDay = document.querySelector('.selectDay')
+            let label = document.querySelector('label')
+
+            selectDay.addEventListener('change', showLabel)
+
+            function showLabel() {
+
+                label.style.display = 'block'
+                // 抓selectDay的值
+                let item = selectDay.value;
+
+                switch (item) {
+                    case "tab_Mon":
+                        label.innerHTML = `${myJson[el - 1].星期一}`
+                        break;
+                    case "tab_Tue":
+                        label.innerHTML = `${myJson[el - 1].星期二}`
+                        break;
+                    case "tab_Wed":
+                        label.innerHTML = `${myJson[el - 1].星期三}`
+                        break;
+                    case "tab_Thr":
+                        label.innerHTML = `${myJson[el - 1].星期四}`
+                        break;
+                    case "tab_Fri":
+                        label.innerHTML = `${myJson[el - 1].星期五}`
+                        break;
+                    case "tab_Sat":
+                        label.innerHTML = `${myJson[el - 1].星期六}`
+                        break;
+                    case "tab_Sun":
+                        label.innerHTML = `${myJson[el - 1].星期日}`
+                        break;
+                    default:
+                        label.innerHTML = ``
+                }
+
+            }
+
         });
 
 }
-
-
-
-// 選擇日期
-$(function () {
-
-    //全部選擇隱藏
-    $('div[id^="tab_"]').hide();
-
-    $('#selectDay').change(function () {
-
-        let sltValue = $(this).val();
-        console.log(sltValue);
-        $('div[id^="tab_"]').hide();
-        //指定選擇顯示
-        $(sltValue).show();
-    });
-
-});
-
 
 
 // 關閉彈跳視窗
@@ -108,4 +121,32 @@ Closebox.addEventListener('click', function () {
     let element = document.getElementById('overlay')
     element.style.display = 'none'
 })
+
+
+// ===================================================================================北部
+
+// ===================================================================================中部
+
+fetch('./text場館.json')
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (myJson) {
+        console.log(myJson);
+        const showVenueNorth = document.querySelector('#showVenueCentral')
+        let html = ''
+        myJson.forEach(element => {
+
+           
+            html += `
+            <div class="col-lg-2">
+                <button class="openBox" onclick="OpenModal(${element.場館編號})">
+                    <img src="${element.場館圖片}" title="${element.場館名稱}">
+                    <p>${element.場館名稱}</p>
+                </button>
+            </div>
+            `;
+        });
+        showVenueNorth.innerHTML += html;
+    });
 
