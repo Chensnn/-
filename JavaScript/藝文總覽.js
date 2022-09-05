3aa71df7d6a54aa42dd6ef4474093d6477e3d93c
Chensn <onlottlr@gmail.com>
2022-09-05 16:18:31 +0800

    藝文總覽

--- JavaScript/藝文總覽.js
+++ JavaScript/藝文總覽.js
@@ -1,14 +1,29 @@
 fetch('./text.json')
-            .then(function (response) {
-                return response.json();
-            })
-            .then(function (myJson) {
-              
-                const table = document.querySelector('#showInfo')
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (myJson) {
 
-                let html = ''
-                myJson.forEach(element => {
-                    html += ``;
-                });
-                table.innerHTML += html;
-            });
+        const table = document.querySelector('#showInfoBk')
+
+        let html = ''
+        myJson.forEach(element => {
+            html += `
+    
+    
+            <div class="col-lg-3">
+                <figure>
+                    <div class="imgCard">
+                        <img src="${element.展演圖片}">
+                        <div class="overlay">
+                            <div class="text">
+                                <p>${element.展演介紹}</p>
+                            </div>
+                        </div>
+                        <a href="${element.購票連結}" title="前往購票"><figcaption>${element.場館名稱}</br>${element.展演名稱}</figcaption></a>
+                    </div>
+                </figure>
+            </div>`
+});
+        table.innerHTML += html;
+    });
